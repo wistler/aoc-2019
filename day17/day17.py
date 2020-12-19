8cf246e44e125a9e69a0f36131d220b0724a3d75
Sooraj Kumar <the.wistler@gmail.com>
2020-12-18 23:02:54 -0500

    Cleaned up factorizeSteps() implementation

--- day17/day17.py
+++ day17/day17.py
@@ -118,123 +118,104 @@ def nextDir(gr, pt, facingDir):
     raise Exception("Cannot find nextDir", pt, facingDir)
 
 
-def factorizeSteps(steps, fixedSegments=None, debug=False):
+SEG_NAMES = ['A', 'B', 'C']
+SEG_SIZE_MAX = 20
+
+
+def factorizeSteps(steps, segments=None, debug=False):
     """
-        This is the ugliest recursive I've written to date. ** Needs refactoring **
         returns (True, segments) or (False, None)
     """
-    if not fixedSegments:
-        fixedSegments = [[]]
+    if not segments:
+        segments = []
 
     if debug:
-        print("factorize(): called with fixedSegments: \n\t ->{}"
-                .format("\n\t ->".join("["+str(fs)+"]" for fs in fixedSegments)))
+        print("factorize(): called with segments: \n\t ->{}"
+                .format("\n\t ->".join("["+str(s)+"]" for s in segments)))
 
-    main = steps
-    segments = fixedSegments + [[]] * (4 - len(fixedSegments))
-    
-    segOpts = []
-    mainStr = ",".join(main)
-
-    segNames = ['', 'A', 'B', 'C']
+    mainFn = ",".join(steps)
+    stepPos = 0
     while True:
         changed = False
-        for s in range(1,len(fixedSegments)):
-            segStr = ",".join(segments[s])
-            mainStr = mainStr.replace(segStr, segNames[s])
+        for s in range(len(segments)):
+            segFn = ",".join(segments[s])
+            mainFn = mainFn.replace(segFn, SEG_NAMES[s])
 
             # Avoid portions matched by previous segments..
             # Eg: A,A,...
-            while ",".join(steps).startswith(segStr):
-                steps = steps[len(segments[s]):]
+            while ",".join(steps[stepPos:]).startswith(segFn):
+                stepPos += len(segments[s])
                 changed = True
         if not changed:
             break
 
-    searchStr = mainStr
+    searchPos = 0
     while True:
         changed = False
-        for s in range(1,len(fixedSegments)):
-            while searchStr.startswith(segNames[s]):
-                searchStr = searchStr[2:]
+        for s in range(len(segments)):
+            while mainFn[searchPos:].startswith(SEG_NAMES[s]):
+                searchPos += 2  # skip past "A," or "B," or "C,"
                 changed = True
         if not changed:
             break
-    
 
-    if debug:
-        print("factorize(): main: {}\n\tsteps: {}\n\tsearchStr: {}".format(mainStr, ",".join(steps), searchStr))
-
-
-    for s in range(len(fixedSegments),4):
-        start = 0
+    if len(segments) == len(SEG_NAMES):
         if debug:
-            print("s: {}, start: {}".format(s, start))
+            print("factorize(): validating segment formation .. ")
 
-        for i in range(start+2,len(steps),2):
-            seg = steps[start:i]
-            segStr = ",".join(seg)
+        factorized = [
+            mainFn,
+            ','.join(segments[0]),
+            ','.join(segments[1]),
+            ','.join(segments[2]),
+        ]
 
-            if len(segStr) > 20 or not searchStr.startswith(segStr):
-                break
+        remainingMainFn = mainFn
+        for t in SEG_NAMES + [',']:
+            remainingMainFn = remainingMainFn.replace(t, '')
 
-            count = mainStr.count(segStr)
-
-            if debug:
-                print("s: {}, {}:{}, c: {}, seg: {}".format(s, start, i, count, segStr))
-
-            if count >= 1:
-                segOpts.insert(0, seg)
-
-        if debug:
-            print("factorize(): options: \n\t{}".format("\n\t".join(str(so) for so in segOpts)))
-
-        for so in segOpts:
-            valid, seg = factorizeSteps(
-                main,
-                fixedSegments + [so], 
-                debug=debug
-            )
-            if valid:
-                return True, seg
-        
-        # if we're in this loop, but haven't found any viable options, no path forward.
-        return False, None
-
-    if debug:
-        print("factorize(): validating segment formation .. ")
-
-
-    factorized = [
-        mainStr,
-        ",".join(segments[1]),
-        ",".join(segments[2]),
-        ",".join(segments[3]),
-    ]
-
-    remainingMainStr = mainStr
-    for t in ["A", "B", "C", ","]:
-        remainingMainStr = remainingMainStr.replace(t, '')
-
-    if len(remainingMainStr) != 0:
-        if debug:
-            print("factorize(): incomplete: {}".format(mainStr))
-        return False, None
-
-    if debug:
-        print("factorize(): factorized segments: \n\t =>{}"
-                .format("\n\t =>".join("["+fs+"]" for fs in factorized)))
-
-    for f in factorized:
-        if len(f) > 20:
-            if debug:
-                print("factorize(): ** LENGTH ERROR ** : {}".format(f))
+        if len(remainingMainFn) != 0:
             return False, None
 
+        if debug:
+            print("factorize(): factorized segments: \n\t =>{}"
+                    .format("\n\t =>".join("["+fs+"]" for fs in factorized)))
+
+        for f in factorized:
+            if len(f) > SEG_SIZE_MAX:
+                return False, None
+
+        if debug:
+            print("factorize(): FOUND SOLUTION! \n\t ->{}"
+                    .format("\n\t =>".join("["+fs+"]" for fs in factorized)))
+        return True, factorized
+
     if debug:
-        print("factorize(): FOUND SOLUTION! \n\t ->{}"
-                .format("\n\t =>".join("["+fs+"]" for fs in factorized)))
-    return True, factorized
+        print("factorize(): main: {}\n\tsearchStr: {}".format(",".join(steps[stepPos:]), mainFn[searchPos:]))
+
+    segOpts = []
+    for i in range(stepPos+2, len(steps), 2):  # skip in pairs [R,5] or [L,6], etc
+        seg = steps[stepPos:i]
+        segFn = ",".join(seg)
+
+        if len(segFn) > SEG_SIZE_MAX or not mainFn[searchPos:].startswith(segFn):
+            # crossed the limits; not going to find anymore valid options.
+            break
+
+        count = mainFn[searchPos:].count(segFn)
+        if count >= 1:
+            segOpts.append(seg)
+
+    if debug:
+        print("factorize(): options: \n\t{}".format("\n\t".join(str(so) for so in segOpts)))
+
+    for so in segOpts:
+        valid, seg = factorizeSteps(steps, segments + [so], debug=debug)
+        if valid:
+            return True, seg
+    
+    # if we're in this loop, but haven't found any viable options, no path forward.
+    return False, None
 
 
 if __name__ == '__main__':
